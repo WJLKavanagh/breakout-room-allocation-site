74d3a95795341ea13c57d6bc271d1105968e94cf
William <bkav93@gmail.com>
2020-08-27 16:17:20 +0100

    Parse view serving correctly, functionality missing

--- groupings/urls.py
+++ groupings/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:participants>over<int:rounds>', views.query, name='query'),
+    path('download/<int:alloc_id>', views.download, name="to_download"),
+    path('parse/<int:alloc_id>', views.parse, name="to_parse")
 ]
--- groupings/views.py
+++ groupings/views.py
@@ -7,7 +7,6 @@ from .models import Allocation
 def index(request):
     return render(request, 'groupings/index.html', {"allocations":Allocation.objects.all()})
 
-
 def query(request, participants, rounds):
     try:
         allocations = Allocation.objects.filter(num_participants=participants, num_rounds=rounds)
@@ -16,5 +15,18 @@ def query(request, participants, rounds):
     context = {"participants":participants, "rounds":rounds, "allocations":allocations}
     return render(request, "groupings/query.html", context)
 
-def allocation(request, alloc):
-    return render(request, "groupings/allocation.html", None)
+def parse(request, alloc_id):
+    return render(request, "groupings/parse.html", {"alloc":Allocation.objects.get(pk=alloc_id)})
+
+def download(request, alloc_id):
+    # Get the object from the DB
+    try:
+        db_obj = Allocation.objects.get(id=alloc_id) # Check ID is correct fieldname (by default it should be the right one)
+    except Allocation.DoesNotExist:
+        return HttpResponse("Unknown matching ID.", content_type='text/plain')
+    
+    response = HttpResponse(db_obj.matching, content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename="match.txt"'
+    
+    return response
+    
