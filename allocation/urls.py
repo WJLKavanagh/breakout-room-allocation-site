0f8c2ad0e9dbc609ad8f51062c671401f9dfd7ef
Ivaylo Valkov <2064491v@student.gla.ac.uk>
2020-12-01 10:27:19 +0000

    Added redirect from nothing to allocate/

--- allocation/urls.py
+++ allocation/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('allocate/', include('groupings.urls')),
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('allocate/', permanent=False)),
 ]
