75a841ea3ab6ccee920efaa42962e47ec32daabc
Ivaylo Valkov <2064491v@student.gla.ac.uk>
2025-07-14 16:00:41 +0100

    updated database population script

--- populate_db.py
+++ populate_db.py
@@ -9,46 +9,64 @@ import json
 import collections
 
 def main():
+    ## Locate all allocation files
     mypath = "./allocations_data"
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    folders = [f for f in listdir(mypath) if not isfile(join(mypath, f))]
+    #onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    onlyfiles = []
+    for folder in folders:
+        folderpath = join(mypath, folder)
+        #print(listdir(folderpath))
+        for f in listdir(folderpath):
+            filehandle = join(folderpath, f)
+            if isfile(filehandle):
+                onlyfiles.append(filehandle)
+        #print([f for f in listdir(folderpath) if isfile(join(folderpath, f))])
     result = []
     pkindex = 0
     all_correct = True
+    latex_comparison= open("comparison.txt", 'w')
 
-    for filename in onlyfiles:
+    ## Read in allocations from files
+    for filehandle in onlyfiles:
+        filename = filehandle[filehandle.rindex("\\")+1:]
         print("Loading data from: "+filename)
-        pkindex = pkindex+1
-        file = open(mypath+"/"+filename, 'r')
+        pkindex = pkindex+1 #allocation index
+        #file = open(mypath+"/"+filename, 'r')
+        file = open(filehandle, 'r')
         text = "".join(file.readlines()).replace("\n","")
-        matching = json.loads(text)
-        num_rounds = len(matching)
+        matching = json.loads(text) #convert to JSON to store in database
+        num_rounds = len(matching) 
         #print(num_rounds)
         expected_participants = filename.split("_")[1]
         expected_group_sizes = filename.split("(")[1].split(")")[0].split(",")
         #print(num_participants)
-        if not is_correct(matching, expected_participants, expected_group_sizes):
+        if not is_correct(matching, expected_participants, expected_group_sizes,latex_comparison):
             print("Error for matching in {0}".format(filename))
             all_correct = False
             #break
-        num_participants=0
-        for group in matching[0]:
-            for participant in group:
-                num_participants +=1
+        #num_participants=0
+        #for group in matching[0]:
+            #for participant in group:
+                #num_participants +=1
         result.append({
         "model":"groupings.allocation",
         "pk":pkindex,
         "fields":{
-          "num_participants":num_participants,
+          "num_participants":expected_participants,
           "num_rounds":num_rounds,
           "matching":str(matching)
         }})
         file.close()
+        #print("v={0:6s} k={1:6s} r={2:<6d}".format(expected_participants,",".join(expected_group_sizes),num_rounds))
+        latex_comparison.write("{0:s}&{1:s}&{2:d}\n".format(expected_participants,",".join(expected_group_sizes),num_rounds))
 
     # ADD CORRECTNESS CHECK
 
     file = open("matchings.json", "w")
     json.dump(result, file)
     file.close()
+    latex_comparison.close()
     print ("done")
     if all_correct:
         print("Success!")
@@ -56,7 +74,8 @@ def main():
         print("Failures found")
 
 
-def is_correct(matching, expected_participants, expected_group_sizes):
+def is_correct(matching, expected_participants, expected_group_sizes,latex_comparison):
+    ## Find number or participants
     num_participants=0
     for group in matching[0]:
         for participant in group:
@@ -64,9 +83,10 @@ def is_correct(matching, expected_participants, expected_group_sizes):
     if str(num_participants) != expected_participants:
         print("participants are {0} instead of {1}".format(num_participants, expected_participants))
         return False
+    ## Check that blocks are correct
     for participant in range(num_participants):
-        matched_with = []
         # checks whether participant appears exactly once each round
+        matched_with = []
         for roundd in matching:
             present = False
             for group in roundd:
@@ -103,13 +123,14 @@ def is_correct(matching, expected_participants, expected_group_sizes):
     # check that the group sizes are the same as the ones advertised
     for size in group_sizes:
         if size not in expected_group_sizes:
-            print("Group sizes are not as advertised: {0} instead of {1}".format(group_sizes, expected_group_sizes))
+            print("Group sizes are not as advertised: have {0}, expected {1}".format(group_sizes, expected_group_sizes))
             return False
     for size in expected_group_sizes:
         if size not in group_sizes:
-            print("Group sizes are not as advertised: {0} instead of {1}".format(group_sizes, expected_group_sizes))
+            print("Group sizes are not as advertised: have {0}, expected {1}".format(group_sizes, expected_group_sizes))
             return False
 
+    ## Find the two group sizes and the number of each group; and check that they are consistent for reach round
     t_size1, t_num1, t_size2, t_num2 = 0, 0, 0, 0
     for roundd in matching:
         size1, num1, size2, num2 = 0, 0, 0, 0
@@ -155,6 +176,8 @@ def is_correct(matching, expected_participants, expected_group_sizes):
             else:
                 print("Error occurrences of groups differ here- This error case should not happen")
                 return False
+    #print("m1,2={0:>2d},{1:<2d}".format(t_num2, t_num1),end="")
+    latex_comparison.write("{0:d},{1:d}&".format(t_num2, t_num1))
     return True
 
 
